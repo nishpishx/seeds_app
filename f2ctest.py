be65eaa6ac14577df01ac7eed46748a94e2b058d
nishpishx <160370520+nishpishx@users.noreply.github.com>
2025-12-06 12:20:18 -0500

    Refactor path planning and visualization logic
    Refactor robot path planning and visualization for Dubins and Reeds-Shepp curves.

--- f2ctest.py
+++ f2ctest.py
@@ -1,48 +1,66 @@
 import math
 import fields2cover as f2c
 
-# ----------------------------
-# 1. Create robot and field
-# ----------------------------
-rand = f2c.Random(42)             # Random field generator
-robot = f2c.Robot(2.0, 6.0)       # Robot(width, coverage_width)
-robot.setMinTurningRadius(2.0)    # meters
-robot.setMaxDiffCurv(0.1)         # 1/m^2
-
-# Generate a random field with 5 cells
-field = rand.generateRandField(1e4, 5)
-cells = field.getField()
-
-# ----------------------------
-# 2. Generate headlands
-# ----------------------------
+rand = f2c.Random(42)
+robot = f2c.Robot(2.0, 6.0)
+field = rand.generateRandField(1e4, 5);
+cells = field.getField();
 const_hl = f2c.HG_Const_gen()
 no_hl = const_hl.generateHeadlands(cells, 3.0 * robot.getWidth())
-
-# ----------------------------
-# 3. Generate swaths
-# ----------------------------
 bf = f2c.SG_BruteForce()
+
 swaths = bf.generateSwaths(math.pi, robot.getCovWidth(), no_hl.getGeometry(0))
 
-# ----------------------------
-# 4. Sort swaths (snake order)
-# ----------------------------
 snake_sorter = f2c.RP_Snake()
-swaths_sorted = snake_sorter.genSortedSwaths(swaths)
+swaths = snake_sorter.genSortedSwaths(swaths)
+swaths.at(0).getPath().exportToWkt()
 
-# ----------------------------
-# 5. Plan path using Dubins curves
-# ----------------------------
+
+robot.setMinTurningRadius(2)  # m
+robot.setMaxDiffCurv(0.1)  # 1/m^2
 path_planner = f2c.PP_PathPlanning()
-dubins = f2c.PP_DubinsCurves()
-path_dubins = path_planner.planPath(robot, swaths_sorted, dubins)
 
-# ----------------------------
-# 6. Visualize field and path
-# ----------------------------
-f2c.Visualizer.figure()
-#f2c.Visualizer.plotField(field)
-f2c.Visualizer.plot(path_dubins)
-#f2c.Visualizer.show()
-f2c.Visualizer.save("Tutorial_image.png")
+print("####### Tutorial 6.1 Dubins curves ######")
+dubins = f2c.PP_DubinsCurves()
+path_dubins = path_planner.planPath(robot, swaths, dubins);
+
+
+f2c.Visualizer.figure();
+f2c.Visualizer.plot(cells);
+f2c.Visualizer.plot(no_hl);
+f2c.Visualizer.plot(path_dubins);
+f2c.Visualizer.plot(swaths);
+f2c.Visualizer.save("Tutorial_6_1_Dubins.png");
+
+print("\n\n####### Tutorial 6.2 Dubins curves with Continuous curvature ######");
+dubins_cc = f2c.PP_DubinsCurvesCC();
+path_dubins_cc = path_planner.planPath(robot, swaths, dubins_cc);
+
+f2c.Visualizer.figure();
+f2c.Visualizer.plot(cells);
+f2c.Visualizer.plot(no_hl);
+f2c.Visualizer.plot(path_dubins_cc);
+f2c.Visualizer.plot(swaths);
+f2c.Visualizer.save("Tutorial_6_2_Dubins_CC.png");
+
+print("\n\n####### Tutorial 6.3 Reeds-Shepp curves ######")
+reeds_shepp = f2c.PP_ReedsSheppCurves();
+path_reeds_shepp = path_planner.planPath(robot, swaths, reeds_shepp);
+
+f2c.Visualizer.figure();
+f2c.Visualizer.plot(cells);
+f2c.Visualizer.plot(no_hl);
+f2c.Visualizer.plot(path_reeds_shepp);
+f2c.Visualizer.plot(swaths);
+f2c.Visualizer.save("Tutorial_6_3_Reeds_Shepp.png");
+
+print("\n\n####### Tutorial 6.4 Reeds-Shepp curves with Continuous curvature ######")
+reeds_shepp_hc = f2c.PP_ReedsSheppCurvesHC();
+path_reeds_shepp_hc = path_planner.planPath(robot, swaths, reeds_shepp_hc);
+
+f2c.Visualizer.figure();
+f2c.Visualizer.plot(cells);
+f2c.Visualizer.plot(no_hl);
+f2c.Visualizer.plot(path_reeds_shepp_hc);
+f2c.Visualizer.plot(swaths);
+f2c.Visualizer.save("Tutorial_6_4_Reeds_Shepp_HC.png");
