30480104585cfc7fdd2454ec337e874bb6657d1c
nishpishx <160370520+nishpishx@users.noreply.github.com>
2025-12-06 11:56:08 -0500

    Delete polygon mission test code
    Removed polygon mission test code from f2ctest.py.

--- f2ctest.py
+++ f2ctest.py
@@ -1,5 +1,5 @@
 import fields2cover as f2c
-from seed_generator import generate_seed_coords, generate_seed_coords_polygon
+from seed_generator import generate_seed_coords
 
 # -------------------------
 # Test rectangular field
@@ -20,23 +20,4 @@ f2c_points_rect = [f2c.geometry.Point(x, y) for x, y in coords_rect]
 # Visualize rectangle mission
 f2c.visualizer.visualize_coverage_path(f2c_points_rect)
 
-# -------------------------
-# Test polygon mission
-# -------------------------
-polygon_latlon = [
-    (48.3371, -122.55259),
-    (48.3371, -122.55159),
-    (48.3361, -122.55179),
-    (48.3365, -122.5528)
-]
-num_plants = 20
 
-mission_poly = generate_seed_coords_polygon(polygon_latlon, num_plants)
-
-# Extract coordinates for visualization
-coords_poly = [(pt['geometry']['coordinates'][0], pt['geometry']['coordinates'][1])
-               for pt in mission_poly['features'][1:]]  # skip LineString
-f2c_points_poly = [f2c.geometry.Point(x, y) for x, y in coords_poly]
-
-# Visualize polygon mission
-f2c.visualizer.visualize_coverage_path(f2c_points_poly)
