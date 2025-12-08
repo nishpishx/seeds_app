f8b73394354bde52e47aee8cfd214ca5bc7c1fd7
nishpishx <160370520+nishpishx@users.noreply.github.com>
2025-12-08 12:58:40 -0500

    Update f2ctest.py

--- f2ctest.py
+++ f2ctest.py
@@ -34,3 +34,23 @@ except Exception as e:
 print("Area:", cells.area())
 print("Is convex?", cells.isConvex())
 print("GeoJSON preview:", cells.exportToJson())
+
+rand = f2c.Random(42)
+robot = f2c.Robot(2.0, 6.0)
+const_hl = f2c.HG_Const_gen()
+robot.setMinTurningRadius(2)  # m
+robot.setMaxDiffCurv(0.1);  # 1/m^2
+path_planner = f2c.PP_PathPlanning()
+no_hl = const_hl.generateHeadlands(cells, 3.0 * robot.getWidth())
+bf = f2c.SG_BruteForce()
+swaths = bf.generateSwaths(math.pi, robot.getCovWidth(), no_hl.getGeometry(0))
+snake_sorter = f2c.RP_Snake()
+swaths = snake_sorter.genSortedSwaths(swaths)
+dubins = f2c.PP_DubinsCurves()
+path_dubins = path_planner.planPath(robot, swaths, dubins);
+
+
+f2c.Visualizer.figure();
+
+f2c.Visualizer.plot(lines);
+f2c.Visualizer.save("Tutorial_image.png");
