992fa4fb1d0b4d38ee8b2b9f5b3f6e635807cbea
nishpishx <160370520+nishpishx@users.noreply.github.com>
2025-12-08 14:07:36 -0500

    Update seed_generator.py

--- seed_generator.py
+++ seed_generator.py
@@ -33,8 +33,6 @@ def generate_path_csv(perimeter, csv_filename="path.csv"):
     rand = f2c.Random(42)
     robot = f2c.Robot(2.0, 6.0)
     const_hl = f2c.HG_Const_gen()
-    field = rand.generateRandField(1e4, 5);
-    field.setCRS("EPSG:4326");
     robot.setMinTurningRadius(2)  # m
     robot.setMaxDiffCurv(0.1);  # 1/m^2
     path_planner = f2c.PP_PathPlanning()
@@ -45,7 +43,7 @@ def generate_path_csv(perimeter, csv_filename="path.csv"):
     swaths = snake_sorter.genSortedSwaths(swaths)
     dubins = f2c.PP_DubinsCurves()
     path_dubins = path_planner.planPath(robot, swaths, dubins);
-    path_gps = f2c.Transform.transformToPrevCRS(path_dubins, field);
+    path_gps = f2c.Transform.transformPath(path, "local", "EPSG:4326")
     path_gps.saveToFile("path.csv");
     
     f2c.Visualizer.figure();
