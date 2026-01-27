2da6691d0e74463f4b782b9d35aad45c1b24b175
Nisha <nwalvekar@mail.wlu.edu>
2026-01-27 11:05:28 -0500

    boundary josn simplified

--- db_queries.py
+++ db_queries.py
@@ -65,7 +65,7 @@ def get_study_sites_with_sectors():
                     "id": str(sec_id),
                     "name": name,
                     "description": description,
-                    "boundary": coordinates[0]
+                    "boundary": json.loads(boundary) if boundary else None
                 })
         
         return result
